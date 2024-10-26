bb35b5df2e4182feb8e7c71845ea0a042bd3eadb
Juan Pablo <122913492+JPGomezCuervo@users.noreply.github.com>
2024-10-25 19:45:05 -0500

    daytime tcp client finished

--- chap_1/daytimetcpcli.c
+++ chap_1/daytimetcpcli.c
@@ -12,7 +12,8 @@
 
 int main(int argc, char **argv) {
   int sockfd, n;
-  char recvline [MAXLINE+1];
+  char recvline [MAXLINE + 1];
+  struct sockaddr_in servaddr;
   /*
    * int socket (int domain, int type, int protocol);
    *
@@ -35,12 +36,12 @@ int main(int argc, char **argv) {
    *
    * ON_ERROR -> -1.
    */
-  struct sockaddr_in servaddr;
 
   if (argc != 2)
-    printf("Usage: a.out <IPaddress>\n");
+    printf("Usage: ./daytimetcp <IPaddress>\n");
 
-  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+          err_sys("socket error");
 
   /*
    * bzero has to be called with the struct sockaddr_in because it has be initialized
@@ -87,17 +88,14 @@ int main(int argc, char **argv) {
   if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) < 1)
           err_sys("inet_pton error");
 
-  if (sockfd < 0)
-          err_sys("socket error");
-
   if (connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0)
           err_sys("connect error");
 
   while ( (n = read(sockfd, recvline, MAXLINE )) > 0)
   {
-          recvline[n] = '\n';
+          recvline[n] = '\0';
 
-          if(fputs(recvline, stdin) == EOF) 
+          if(fputs(recvline, stdout) == EOF) 
                   err_sys("fputs error");
   }
 

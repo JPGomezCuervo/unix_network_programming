a1f95751220ff089b986887fa9de6d8b98827306
Juan Pablo <122913492+JPGomezCuervo@users.noreply.github.com>
2024-11-10 20:40:47 -0500

    add wrapper for inet_ntop

--- include/unp.h
+++ include/unp.h
@@ -32,3 +32,6 @@ ssize_t Readn(int fd, void *vptr, size_t nbytes);
 ssize_t Writen(int fd, void *vptr, size_t nbytes);
 ssize_t readline(int fd, void *vptr, size_t maxlen); /* cambiar por Readline */
 int Sock_pton_loose(int af, const char *src, void *dst);
+
+/*libwrap*/
+inline char *Inet_ntop(int af, const void *src, char *dst, socklen_t size)

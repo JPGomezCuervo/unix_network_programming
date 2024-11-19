dbb7163e31f4a4845074f3d27891f215abf91b03
Juan Pablo <122913492+JPGomezCuervo@users.noreply.github.com>
2024-11-18 22:21:07 -0500

    implement Fgets, Fputs, and Inet_pton

--- lib/libwrap.c
+++ lib/libwrap.c
@@ -12,3 +12,32 @@ inline const char *Inet_ntop(int af, const void *src, char *dst, socklen_t size)
 
         return ptr;
 }
+
+inline void Inet_pton(int af, const char *pstr, void *dst)
+{
+        int n;
+
+        if ( (n = inet_pton(af, pstr, dst)) < 0)
+                err_sys("inet_pton error for %s\n", pstr);
+        else if (n == 0)
+                err_quit("inet_pton error for %s\n", pstr);
+}
+
+char *Fgets(char *buff, int size, FILE *stream)
+{
+        char *s;
+
+        if ( (s = fgets(buff, size, stream)) == NULL)
+                err_quit("fgets error");
+
+        return s;
+}
+
+int Fputs(char *s, FILE *stream)
+{
+        int n;
+        if ( (n = fputs(s, stream)) < 0)
+                err_quit("fputs error");
+
+        return n;
+}

5a049d33d82584422f0ddf35c7aa6ff0b1b2acaf
Juan Pablo <122913492+JPGomezCuervo@users.noreply.github.com>
2025-01-11 12:28:21 -0500

    add comments

--- chap_5/sigchldwait.c
+++ chap_5/sigchldwait.c
@@ -5,9 +5,17 @@ void sig_chld(int signo)
         pid_t pid;
         int stat;
 
+        /* wait() blocks until a childprocess has terminated, it acknowledges
+         * that a child has finished and retrieves his status */
         pid = wait(&stat);
 
-        /* -1 means wait for any child process */
+        /* we loop on while because we want to check if another child has
+         * terminated at the exact same moment. We use waitpid() because it let
+         * us customized the wait default behavior. We can pass, for example,
+         * WNOHANG for immediately return 0 in case no child has terminated,
+         * hence we say that waitpid() with WNOHANG is non-blocking */
+
+        /* -1 means wait for any child process.*/
         while ( (pid = waitpid(-1, &stat, WNOHANG)) > 0)
                 printf("child %d terminated\n", pid);
 
